ec010ee6ec07601f0f4c13efdb6302a1ba64455c
Reinaldo Ramos <masterrey@gmail.com>
2019-04-24 22:29:43 -0300

    movimento unity chan

---
+++ Assets/Scripts/CameraChan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChan : MonoBehaviour
+{
+    public GameObject player;
+    public float distance;
+    public float height;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+
+        transform.rotation = Quaternion.LookRotation(dir + Vector3.up * height);
+
+        transform.position = Vector3.Lerp(transform.position,
+            player.transform.position - player.transform.forward*distance + Vector3.up*height,Time.smoothDeltaTime);
+    }
+}
---
+++ Assets/Scripts/MoveChan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChan : MonoBehaviour
+{
+    CharacterController ctrl;
+    Animator anim;
+    Vector3 inputCtrl;
+    Vector3 inputRotCtrl;
+    void Start()
+    {
+        ctrl = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputCtrl = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        inputRotCtrl = new Vector3(0,Input.GetAxis("Horizontal"), 0);
+    }
+
+    private void FixedUpdate()
+    {
+        ctrl.SimpleMove(transform.TransformDirection(inputCtrl*3));
+        anim.SetFloat("Speed", ctrl.velocity.magnitude);
+        transform.Rotate(inputRotCtrl);
+    }
+}
