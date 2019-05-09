f5c491151043460e3b8ff9ffc35f18bd9ce42d41
Reinaldo Augusto de Oliveira Ramos <reinaldo.ramos@espm.br>
2019-05-08 22:10:39 -0300

    Day light and camera block

--- Assets/Scripts/CameraChan.cs
+++ Assets/Scripts/CameraChan.cs
@@ -7,6 +7,7 @@ public class CameraChan : MonoBehaviour
     public GameObject player;
     public float distance;
     public float height;
+    public float ajust;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -16,10 +17,17 @@ public class CameraChan : MonoBehaviour
     void LateUpdate()
     {
         Vector3 dir = player.transform.position - transform.position;
+        Vector3 mygoto = player.transform.position - player.transform.forward * distance + Vector3.up * height;
 
+        RaycastHit hit;
+        if(Physics.Raycast(player.transform.position+ Vector3.up * height, player.transform.forward * (-distance) + Vector3.up * height,out hit, 10))
+        {
+            mygoto = hit.point+hit.normal*ajust;
+        }
+
+       
         transform.rotation = Quaternion.LookRotation(dir + Vector3.up * height);
 
-        transform.position = Vector3.Lerp(transform.position,
-            player.transform.position - player.transform.forward*distance + Vector3.up*height,Time.smoothDeltaTime);
+        transform.position = Vector3.Lerp(transform.position,mygoto,Time.smoothDeltaTime);
     }
 }
---
+++ Assets/Scripts/DayCicle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCicle : MonoBehaviour
+{
+    float dayseconds;
+    float anglight;
+    public float dayspeed=1;
+    Light mylight;
+    float intensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mylight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dayseconds += Time.deltaTime* dayspeed; 
+        anglight = (dayseconds / (84600 / 2)) * 180 / Mathf.PI;
+        intensity = Vector3.Dot(transform.forward, Vector3.down);
+        mylight.intensity = intensity;
+        transform.rotation = Quaternion.Euler(anglight, -30, 0);
+    }
+}
